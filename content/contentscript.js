ab328b1de0e4bc3a6f661e3da1b1cd143c7e533f
jason-zhao-git <zjq233333@gmail.com>
2023-06-02 21:43:31 -0700

    icon finished

--- content/contentscript.js
+++ content/contentscript.js
@@ -1,3 +1,4 @@
+
 let youtubeRightControls = document.getElementsByClassName("ytp-right-controls");
 
 // ============= Add Button to the Right control panel =================== //
@@ -19,12 +20,12 @@ svg.setAttribute("fill-opacity", "1");
 
 const useElement = document.createElementNS("http://www.w3.org/2000/svg", "use");
 
-useElement.setAttribute("xlink:href", "#ytp-id-17");
+useElement.setAttribute("xlink:href", "#ytp-id-09808");
 
 const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-path.setAttribute("d", "M11,11 C9.89,11 9,11.9 9,13 L9,23 C9,24.1 9.89,25 11,25 L25,25 C26.1,25 27,24.1 27,23 L27,13 C27,11.9 26.1,11 25,11 L11,11 Z M17,17 L15.5,17 L15.5,16.5 L13.5,16.5 L13.5,19.5 L15.5,19.5 L15.5,19 L17,19 L17,20 C17,20.55 16.55,21 16,21 L13,21 C12.45,21 12,20.55 12,20 L12,16 C12,15.45 12.45,15 13,15 L16,15 C16.55,15 17,15.45 17,16 L17,17 L17,17 Z M24,17 L22.5,17 L22.5,16.5 L20.5,16.5 L20.5,19.5 L22.5,19.5 L22.5,19 L24,19 L24,20 C24,20.55 23.55,21 23,21 L20,21 C19.45,21 19,20.55 19,20 L19,16 C19,15.45 19.45,15 20,15 L23,15 C23.55,15 24,15.45 24,16 L24,17 L24,17 Z");
+path.setAttribute("d", "m9.25,11.75a1.25,1.25 0 1 0 0,2.5a1.25,1.25 0 0 0 0,-2.5zm3.75,0.25a1,1 0 0 0 0,2l12,0a1,1 0 1 0 0,-2l-12,0zm0,5a1,1 0 1 0 0,2l14,0a1,1 0 1 0 0,-2l-14,0zm-1,6a1,1 0 0 1 1,-1l10,0a1,1 0 1 1 0,2l-10,0a1,1 0 0 1 -1,-1zm-4,-5a1.25,1.25 0 1 1 2.5,0a1.25,1.25 0 0 1 -2.5,0zm1.25,3.75a1.25,1.25 0 1 0 0,2.5a1.25,1.25 0 0 0 0,-2.5z");
 path.setAttribute("fill", "#fff");
-path.setAttribute("id", "ytp-id-17");
+path.setAttribute("id", "ytp-id-09808");
 
 svg.appendChild(useElement);
 svg.appendChild(path);
@@ -37,4 +38,21 @@ youtubeRightControls[0].insertBefore(switchBtn, youtubeRightControls[0].firstEle
 
 //const element = document.querySelector('.ytp-subtitles-button.ytp-button.ytp-danmaku-button');
 
+// ======== Change icon color ======== //
+let defaultcolor = 0;
+switchBtn.onclick = () => {
+
+    let svg = switchBtn.querySelector('svg');
+    let path = svg.querySelector('path');
+    
+    if (defaultcolor === 0) {
+        path.setAttribute('fill', 'green');
+        defaultcolor = 1;
+    } else {
+        path.setAttribute('fill', '#fff');
+        defaultcolor = 0;
+    }
+    
+    
+}
 
